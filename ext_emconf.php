1a6d872ee3ac47873e80db82072d09c5c02e7f65
Steffen Kroggel <kroggel@rkw.de>
2023-03-28 11:56:57 +0200

    [FIX] Fixing typo in dependencies

--- ext_emconf.php
+++ ext_emconf.php
@@ -24,7 +24,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '9.5.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-7.6.99',
+            'typo3' => '9.5.0-9.6.99',
             'core_extended' => '9.5.4-9.5.99',
         ],
         'conflicts' => [],
